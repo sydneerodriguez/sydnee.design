5b9453f454085fffb0458a825914428419d60ab1
aidant <15520814+aidant@users.noreply.github.com>
2020-08-12 22:47:48 +1000

    Use sydnee.design

--- vue.config.js
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { name } = require('./package.json')
-
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? `/${name}/` : '/',
-}
