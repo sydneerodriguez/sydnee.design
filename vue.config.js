07581b71d21c764ec433ab8236fb748adbd281bd
aidant <15520814+aidant@users.noreply.github.com>
2021-02-10 00:18:35 +1100

    Add Placeholder

---
+++ vue.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  publicPath: '/7f277138a581354e680cabbc55642148495feca6c47057b38864d4fd643408cc/',
+  outputDir: './dist/7f277138a581354e680cabbc55642148495feca6c47057b38864d4fd643408cc',
+  lintOnSave: false,
+}
