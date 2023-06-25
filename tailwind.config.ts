26a6ae25def2731b4e8a8f8775d0be52f3fd7a26
aidant <15520814+aidant@users.noreply.github.com>
2023-06-25 15:14:09 +1000

    move styles to tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,19 @@
 import type { Config } from 'tailwindcss'
+import theme from 'tailwindcss/defaultTheme'
 
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        // custom title font
+        title: ['Poppins', ...theme.fontFamily.sans],
+
+        sans: ['Roboto', ...theme.fontFamily.sans],
+        serif: [...theme.fontFamily.serif],
+        mono: [...theme.fontFamily.mono],
+      },
+    },
   },
   plugins: [],
 } satisfies Config
